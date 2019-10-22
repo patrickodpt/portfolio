105beed1ad3738422c197bb43e50f8a80d5437f0
Patrick O'Shea <patrickodpt@gmail.com>
2019-10-22 16:09:57 -0400

    basic portfolio finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { CssBaseline } from '@material-ui/core'
 import Home from './Components/Home.js'
 import './App.css';
 
+console.log("HEY! If you're looking at this you probably know about a decent job for an entry level developer. If so, hit me up on linkedIn. :)");
+
 function App() {
   return (
     <Fragment>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 // import MyHeader from './Header.js'
-import { Box, Grid, Typography, Link } from '@material-ui/core';
+import MyResume from './Resume.js'
+import { Box, Grid, Typography, Link, Button } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import DescriptionIcon from '@material-ui/icons/Description';
 const github = require('simple-icons/icons/github');
 
 const styles = {
@@ -16,6 +18,13 @@ const styles = {
   EmbiggenedIcon: {
     width: '100px',
     height: '100px',
+    padding: '5px',
+    margin: '10px',
+  },
+  TypoBox: {
+    alignItems: 'center',
+    display: 'flex',
+    alignSelf: 'start',
   }
 }
 
@@ -28,6 +37,10 @@ function GithubIcon(props) {
 }
 
 class Home extends Component {
+  state = {
+    resumeIsOpen: false,
+  }
+
   render() {
     return (
       <Grid container justify="space-around">
@@ -39,6 +52,7 @@ class Home extends Component {
                 fontWeight: 500,
                 textAlign: 'center',
                 textShadow: '2px 4px black',
+                backgroundColor: '#00000073',
                 }}>
               PATRICK O'SHEA
             </Typography>
@@ -46,37 +60,43 @@ class Home extends Component {
         </Grid>
         <Grid item xs={12} style={styles.GridItem} alignItems='flex-start'>
           <Box boxShadow={2}>
-            <Typography variant='h5'>
+            <Typography variant='h5'
+              style={{
+                fontWeight: 300,
+                textAlign: 'center',
+                textShadow: '2px 4px black',
+                backgroundColor: '#00000073',
+                }}>
               Software Developer // Physical Therapist
             </Typography>
           </Box>
         </Grid>
         <Grid item xs={12} style={styles.GridItem}  alignItems='center'>
-          <Typography>
-            <Link
+          <Typography style={styles.TypoBox}>
+            <Button
               href="https://www.linkedin.com/in/patrickodpt/"
               target="_blank"
               >
               <LinkedInIcon
                 style={styles.EmbiggenedIcon}
                 color="action" />
-            </Link>
-            <Link
-              href="mailto:patrickodpt@gmail.com"
-              target="_blank"
-              >
-              <EmailIcon
-                style={styles.EmbiggenedIcon}
-                color="action" />
-            </Link>
-            <Link
+            </Button>
+            <Button
               href="https://github.com/patrickodpt"
               target="_blank">
               <GithubIcon
                 color="action"
                 style={styles.EmbiggenedIcon}
               />
-            </Link>
+            </Button>
+            <MyResume stylesProp={styles.EmbiggenedIcon}/>
+            <Button
+              href="mailto:patrickodpt@gmail.com"
+              >
+              <EmailIcon
+                style={styles.EmbiggenedIcon}
+                color="action" />
+            </Button>
           </Typography>
         </Grid>
       </Grid>
